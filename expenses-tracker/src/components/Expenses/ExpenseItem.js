175667f0a3dbde412f43c6c07f6f0fcc86a216f8
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-02-22 03:01:32 +0600

    03 - React Basics & Working With Components

---
+++ expenses-tracker/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseData';
+import Card from '../UI/Card';
+
+const ExpenseItem = (props) => {
+    // const expenseDate = new Date(2021, 2, 28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
+    // const month = props.date.toLocaleString('en-US', { month: 'long' });
+    // const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    // const year = props.date.getFullYear();
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    );
+}
+
+export default ExpenseItem;
