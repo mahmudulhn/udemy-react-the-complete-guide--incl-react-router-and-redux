8c79f48d477c126aaa5740e94274c60abb541264
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-04-13 15:54:32 +0600

    012 Adding a Cart Context

--- food-order -app/src/components/Cart/Cart.js
+++ food-order -app/src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
 
 const Cart = props => {
     const cartItems = <ul className={classes['cart-items']}>{[{
@@ -11,17 +12,22 @@ const Cart = props => {
     }].map((item) => <li>{item.name}</li>)}</ul>;
 
     return (
-        <div>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button
+                    className={classes['button--alt']}
+                    onClick={props.onClose}
+                >
+                    Close
+                </button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
