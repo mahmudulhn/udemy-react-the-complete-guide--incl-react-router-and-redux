1674d114c8e84f36a4cd28cdf23add637ebbd0ad
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-04-15 21:20:21 +0600

    12 - Practice Project Building a Food Order App

--- food-order -app/src/components/Layout/HeaderCartButton.js
+++ food-order -app/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import classes from './HeaderCartButton.module.css';
 import CartContext from "../../store/cart-context";
@@ -6,13 +6,33 @@ import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
