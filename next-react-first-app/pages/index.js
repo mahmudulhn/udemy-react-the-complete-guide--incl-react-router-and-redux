7c68aba567f19cbbbe867dcd5ce6723b8a6f3d1d
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-04-29 20:42:08 +0600

    013 Onwards to a bigger Project!

--- next-react-first-app/pages/index.js
+++ next-react-first-app/pages/index.js
@@ -1,6 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
 
 function HomePage() {
-    return <h1>The Home Page</h1>
+    return <Fragment>
+        <h1>The Home Page</h1>
+        <ul>
+            <li>
+                <Link href='./news/next-is-a-great-framework'>
+                    NextJS is a great framework
+                </Link>
+            </li>
+            <li>Something Else</li>
+        </ul>
+    </Fragment>
 }
 
 export default HomePage;
